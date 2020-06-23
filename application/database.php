f7426c6490a3ba83a33a79074fd24b1f494e5215
18695772312 <861454974@qq.com>
2020-06-23 10:10:49 +0800

    5

--- application/database.php
+++ application/database.php
@@ -13,9 +13,9 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.105.109.124',
     // 数据库名
-    'database'        => 'wjx',
+    'database'        => 'user',
     // 用户名
     'username'        => 'root',
     // 密码
