99abbf4675df3fb19856dfab2b7909f7af77076d
18695772312 <861454974@qq.com>
2020-06-23 09:43:25 +0800

    5

---
+++ runtime/temp/9b1428d62858b5bdfb230d86b8e42f80.php
@@ -0,0 +1,13 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:81:"D:\phpstudy_pro\WWW\thinkPHP\public/../application/center\view\center\center.html";i:1592297852;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>个人中心</h1>
+  <a href="<?php echo url('index/index/index'); ?>">跳转</a>
+</body>
+</html>
