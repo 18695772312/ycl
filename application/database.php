4dcbd9f7f165a8c7cabd71269ef4c18e13d872a2
18695772312 <861454974@qq.com>
2020-06-23 10:24:43 +0800

    5

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '47.105.109.124',
     // 数据库名
-    'database'        => 'user',
+    'database'        => 'project',
     // 用户名
     'username'        => 'root',
     // 密码
